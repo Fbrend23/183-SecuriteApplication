0f0906b35e376d1da2d15b7884034ea562568dd6
Brendan Fleurdelys <158816892+Fbrend23@users.noreply.github.com>
2025-11-13 11:18:11 +0100

    ajout du register

--- exercices/auth/nodejstoken/app/routes/Auth.mjs
+++ exercices/auth/nodejstoken/app/routes/Auth.mjs
@@ -1,6 +1,6 @@
 import express from "express";
-import { connectToDatabase } from "../utils/dbUtils.mjs";
 import bcrypt from "bcrypt";
+import { connectToDatabase } from "../utils/dbUtils.mjs";
 
 const router = express.Router();
 
@@ -15,7 +15,26 @@ const connectToDatabaseMiddleware = async (req, res, next) => {
   }
 };
 
+router.post("/register", connectToDatabaseMiddleware, async (req, res) => {
+  const { username, password } = req.body;
+  const pepper = process.env.PEPPER_SECRET;
+
+  const hashedPassword = await bcrypt.hash(password + pepper, 10);
+
+  const insertQuery =
+    "INSERT INTO t_users (useName, usePassword) VALUES (?, ?)";
+
+  try {
+    await req.dbConnection.query(insertQuery, [username, hashedPassword]);
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    console.error("Error registering user:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 router.post("/", connectToDatabaseMiddleware, async (req, res) => {
+  const pepper = process.env.PEPPER_SECRET;
   const { username, password } = req.body;
 
   const queryString = "SELECT * FROM t_users WHERE useName = ?";
@@ -28,7 +47,10 @@ router.post("/", connectToDatabaseMiddleware, async (req, res) => {
     }
 
     const user = rows[0];
-    const passwordMatch = await bcrypt.compare(password, user.usePassword); // compare hashed password
+    const passwordMatch = await bcrypt.compare(
+      password + pepper,
+      user.usePassword
+    ); // compare hashed password
 
     if (!passwordMatch) {
       return res.status(401).json({ error: "Invalid username or password" });
